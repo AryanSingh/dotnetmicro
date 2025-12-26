b01d06a59d6641e39ad8eb47a851b683b23f1b0d
aryan singh <aryan@payzli.com>
2025-12-26 19:28:45 +0530

    catalog minimal apis created

---
+++ eshop-microservices/Services/Catalog/CatalogApi/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+
+var app = builder.Build();
+
+app.MapCarter();
+
+
+
+app.Run();
