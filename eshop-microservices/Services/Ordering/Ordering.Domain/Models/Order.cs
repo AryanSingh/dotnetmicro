d8edbca7090798e3df29b4e1ffcef39317dc9a54
aryan singh <aryan@payzli.com>
2026-02-19 15:18:59 +0530

    integrating domain events

--- eshop-microservices/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ eshop-microservices/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -1,11 +1,11 @@
 namespace Ordering.Domain.Models;
 
-public class Order: Aggregate<Guid>
+public class Order: Aggregate<OrderId>
 {
     private readonly List<OrderItem> _orderItems = new();
     public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
-    public Guid CustomerId { get; private set; } = default!;
-    public string OrderName { get; private set; }  = default!;
+    public CustomerId CustomerId { get; private set; } = default!;
+    public OrderName OrderName { get; private set; }  = default!;
     public Address ShippingAddress { get; private set; }  = default!;
     public Address BillingAddress { get; private set; } = default!;
     public Payment Payment { get; private set; }  = default!;
@@ -16,4 +16,51 @@ public class Order: Aggregate<Guid>
         get => OrderItems.Sum(x => x.Price * x.Quantity);
         private set { }
     }
+    
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+      
+
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending
+        };
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+        return order;
+    }
+    
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+        AddDomainEvent(new OrderUpdatedEvent(this));
+    }
+    
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+        _orderItems.Add(orderItem);
+        AddDomainEvent(new OrderItemAddedEvent(this, orderItem));
+    }
+    
+     public void Remove(OrderItemId orderItemId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(x => x.Id == orderItemId);
+        if (orderItem != null)
+        {
+            _orderItems.Remove(orderItem);
+            AddDomainEvent(new OrderItemRemovedEvent(this, orderItem));
+        }
+    }
+    
 }
--- eshop-microservices/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ eshop-microservices/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -7,4 +7,29 @@ public record Payment
     public DateTime Expiration { get;  }
     public string CVV { get; } = default!;
     public int PaymentMethod { get; } = default!;
+    
+    protected Payment(){}
+    
+    private Payment(string cardNumber, string cardName, DateTime expiration, string cvv, int paymentMethod)
+    {
+        CardNumber = cardNumber;
+        CardName = cardName;
+        Expiration = expiration;
+        CVV = cvv;
+        PaymentMethod = paymentMethod;
+    }
+    
+    public static Payment Of(string cardNumber, string cardName, DateTime expiration, string cvv, int paymentMethod)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+        if (expiration < DateTime.UtcNow)
+        {
+            throw new DomainException("Expiration date cannot be in the past.");
+        }
+        
+        return new Payment(cardNumber, cardName, expiration, cvv, paymentMethod);
+    }
 }
