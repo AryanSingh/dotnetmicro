d90c704cc79ed739947c161bfd8eeac9f6ef3110
aryan singh <aryan@payzli.com>
2026-03-12 22:40:10 +0530

    stashing

--- eshop-microservices/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderUpdatedEventHandler.cs
+++ eshop-microservices/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderUpdatedEventHandler.cs
@@ -1,6 +1,10 @@
 namespace Ordering.Application.Orders.EventHandlers;
 
-public class OrderUpdatedEventHandler_c
+public class OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger): INotificationHandler<OrderUpdatedEvent>
 {
-    
+    public Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+       logger.LogInformation($"Domain event handled: {notification.GetType().Name}");
+       return Task.CompletedTask;
+    }
 }
--- eshop-microservices/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ eshop-microservices/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -19,8 +19,6 @@ public class Order: Aggregate<OrderId>
     
     public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
     {
-      
-
         var order = new Order
         {
             Id = id,
